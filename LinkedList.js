0d7b55fe3f04302d284b1f1d9967d710bd20c5b7
Matthew Hu <matthu24@gmail.com>
2018-01-30 11:43:59 -0800

    Add remove to the SLL

--- LinkedList.js
+++ LinkedList.js
@@ -59,7 +59,29 @@ LinkedList.prototype.push = function(val) {
 // case -2: your targeted node is in the tail. you just have to remove it from the tail. Hence next of the node before the tail will be null.
 // case-3: your targeted node is in the middle of the LinkedList, you have to make the node after your targeted node to be the next node of the node before your targeted node.
 LinkedList.prototype.remove = function(val) {
-  if (this.head.value === val) {
-
+  let current = this.head
+  //if head is the specified value
+  if (current.value === val) {
+    let newHead = current.next
+    this.head = newHead;
+  }else{
+    //loop through list
+    let previous = current;
+    while (current.next) {
+      current = current.next
+      if (current.value == val) {
+        //we've found the specified value in the middle of the list
+        //we want to get rid of it
+        previous.next = current.next
+        break;
+      }
+    }
+    //at this point, if we've exited the loop, which could mean two things:
+    //1. we reached the end of the list
+    //2. we found the value and deleted it
+    //let's make a conditional for the last node in the list
+    if (current.value === val) {
+      previous.next = null;
+    }
   }
 }
