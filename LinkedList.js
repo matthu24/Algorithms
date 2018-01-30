d4186eea49bc771ce389b98d71ac473a5bf0d1a6
Matthew Hu <matthu24@gmail.com>
2018-01-30 11:22:11 -0800

    Add push to SLL

--- LinkedList.js
+++ LinkedList.js
@@ -1,11 +1,9 @@
 //implement a stack
 //js already has stack built into array class
-
 let stack = [];
 stack.push(1);
 stack.push(2);
 stack.push(3);
-
 stack.pop(); //3
 stack.pop(); //2
 stack.pop(); //1
@@ -18,17 +16,50 @@ stack.pop(); //1
 
 //Implement a queue
 //first in first out
-
 let queue = [];
 queue.push(1);
 queue.push(2);
 queue.push(3);
-
 //queue is now [1,2,3]
 //need to take the first off
-
 queue.shift();//1
 queue.shift();//2
 queue.shift();//3
-
 //queue is now empty
+
+
+
+
+//create a singly linked list
+function LinkedList(){
+  this.head = null;
+}
+
+LinkedList.prototype.push = function(val) {
+  var node = {
+    value: val,
+    next: null
+  }
+  //this.head doesn't exist yet
+  if (!this.head) {
+    this.head = node;
+  }else{
+    //this.head exists already
+    //loop to last in linkedlist
+    let current = this.head;
+    while (current.next) {
+      current = current.next
+    }
+    current.next = node
+  }
+}
+//Removing a node-  first you need to find it
+//If you want to remove a node from your linked list you have to find the node. There are three conditions here
+// case -1: your targeted node is in the head. you have to replace the head with the next node
+// case -2: your targeted node is in the tail. you just have to remove it from the tail. Hence next of the node before the tail will be null.
+// case-3: your targeted node is in the middle of the LinkedList, you have to make the node after your targeted node to be the next node of the node before your targeted node.
+LinkedList.prototype.remove = function(val) {
+  if (this.head.value === val) {
+
+  }
+}
