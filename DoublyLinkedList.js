d6de51d6767ba9fc98ed4c22c51439c5d17009b1
Matthew Hu <matthu24@gmail.com>
2018-01-31 12:32:41 -0800

    Add reverse DLL

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,6 +1,5 @@
 function DLinkedList(){
   this.head = null;
-
 }
 
 DLinkedList.prototype.push = function(val){
@@ -15,14 +14,37 @@ DLinkedList.prototype.push = function(val){
     }
   }else{
     //traverse the list until current is null
-    //when current is null, that means previous is the last node.  we will push onto previous' next 
+    //when current is null, that means previous is the last node.  we will push onto previous' next
     current = current.next
     while (current) {
       current = current.next
       previous = previous.next
     }
-
     previous.next = {value: val, previous: previous, next: null}
-
   }
+}
+
+
+
+//reverse doubly linked list without using space
+//traverse list and switch previous with next for each node
+//when reach the end, assign the last node to this.head
+function reverseDLL(dll) {
+  let current = dll.head;
+  let temp;
+  while (current) {
+    //switch
+    temp = current.next;
+    current.next = current.previous;
+    current.previous = temp;
+
+    //assign the last node in list as head
+    if (!temp) {
+      dll.head = current;
+    }
+
+    //at the end of the list, the current will be null and break out of loop
+    current = temp;
+  }
+  return dll;
 }
