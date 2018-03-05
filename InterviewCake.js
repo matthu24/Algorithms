4b87b110ab413c5a87bd35a146c41758e9e546a4
Matthew Hu <matthu24@gmail.com>
2018-03-05 12:37:20 -0800

    Add meeting merge

--- InterviewCake.js
+++ InterviewCake.js
@@ -1,4 +1,4 @@
-//Example of Greedy approach!
+//Example of Greedy approach!  "best answer so far", may also need "additional information"
 //iterate through once, grab the best profit for each iteration
 //we need to track additional information: in this case the lowest price insofar
 function getMaxProfit(stockPrices) {
@@ -60,3 +60,153 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
 
 const intArray = [1, 7, 3, 4];
 console.log(getProductsOfAllIntsExceptAtIndex(intArray));
+
+
+
+
+
+//greedy approach: assume we already have the other two highest numbers
+//each iteration we would just see if the current element gave
+//a higher product than the current max
+function highestProductOf33(arrayOfInts) {
+
+    //we need to keep track of the lowest three and the highest two
+    //because the lowest two could be negative and their product positive
+    let first,second,third,lowest,secondLowest;
+    for(let i=0;i< arrayOfInts.length;i++){
+        let el = arrayOfInts[i]
+        //get first three highest
+        if(!first || el > first){
+            third = second;
+            second = first;
+            first = el;
+        }else if(!second || el > second){
+            third = second;
+            second = el;
+        }else if(!third || el > third){
+            third = el;
+        }
+
+        //get the lowest and second lowest
+        if(!lowest || el < lowest ){
+            secondLowest = lowest;
+            lowest = el;
+        }else if(!secondLowest || el < secondLowest){
+            secondLowest = el;
+        }
+    }
+
+
+    return Math.max(first*lowest*secondLowest,first*second*third)
+}
+const arrayOfInts = [5,6,1,2,-1,-11];
+console.log(highestProductOf33(arrayOfInts));
+
+
+
+
+//interview cake solution :]
+function highestProductOf3(arrayOfInts) {
+
+    if (arrayOfInts.length < 3) {
+        throw new Error('Less than 3 items!');
+    }
+
+    // we're going to start at the 3rd item (at index 2)
+    // so pre-populate highests and lowests based on the first 2 items.
+    // we could also start these as null and check below if they're set
+    // but this is arguably cleaner
+    var highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+    var lowest  = Math.min(arrayOfInts[0], arrayOfInts[1]);
+
+    var highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+    var lowestProductOf2  = arrayOfInts[0] * arrayOfInts[1];
+
+    // except this one--we pre-populate it for the first *3* items.
+    // this means in our first pass it'll check against itself, which is fine.
+    var highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+    // walk through items, starting at index 2
+    for (var i = 2; i < arrayOfInts.length; i++) {
+        var current = arrayOfInts[i];
+
+        // do we have a new highest product of 3?
+        // it's either the current highest,
+        // or the current times the highest product of two
+        // or the current times the lowest product of two
+        highestProductOf3 = Math.max(
+            highestProductOf3,
+            current * highestProductOf2,
+            current * lowestProductOf2
+        );
+
+        // do we have a new highest product of two?
+        highestProductOf2 = Math.max(
+            highestProductOf2,
+            current * highest,
+            current * lowest
+        );
+
+        // do we have a new lowest product of two?
+        lowestProductOf2 = Math.min(
+            lowestProductOf2,
+            current * highest,
+            current * lowest
+        );
+
+        // do we have a new highest?
+        highest = Math.max(highest, current);
+
+        // do we have a new lowest?
+        lowest = Math.min(lowest, current);
+    }
+
+    return highestProductOf3;
+}
+
+
+
+//this is potentially O(n^2)
+function mergeRanges(meetings) {
+    var result = [];
+    // merge meeting ranges
+    let added = false;
+    meetings.forEach(curr => {
+       for(let i = 0; i < result.length; i++){
+          if(curr.startTime <= result[i].endTime && curr.endTime >= result[i].startTime){
+              result[i].endTime = Math.max(curr.endTime, result[i].endTime)
+              result[i].startTime = Math.min(curr.startTime, result[i].startTime)
+
+              added = true;
+          }
+        //   }else if(curr.endTime >= result[i].startTime){
+        //       result[i].startTime = Math.min(curr.startTime, result[i].startTime)
+        //       added = true;
+        //   }
+       }
+       if(added === false){
+           result.push(curr)
+       }
+       added = false;
+
+
+    })
+
+    return result;
+}
+
+const meetings = [
+     {startTime: 1, endTime: 10},
+    {startTime: 2, endTime: 6},//9:00 to 9:30
+     {startTime: 3, endTime: 5},//10:30 to 11:30
+    {startTime: 7, endTime: 9},
+
+
+];
+
+console.log(mergeRanges(meetings));
+
+
+
+
+//do it again but by sorting the meetings and trying to merge with the previous one 
